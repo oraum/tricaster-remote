523ba2b68266299cb2580859808617db2bc6150c
oraum <oraum@users.noreply.github.com>
2020-05-06 00:03:24 +0200

    gh pages Initial Commit

---
+++ precache-manifest.d88eaa771e3e442d4a5dcb059cc26143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9394b82533816f9696f4d631d6f6bd01",
+    "url": "/tricaster-remote/index.html"
+  },
+  {
+    "revision": "7107aa07b997730f86ee",
+    "url": "/tricaster-remote/static/css/main.256eac7b.chunk.css"
+  },
+  {
+    "revision": "a8b008e09b1db9082c74",
+    "url": "/tricaster-remote/static/js/2.3dbda4e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tricaster-remote/static/js/2.3dbda4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7107aa07b997730f86ee",
+    "url": "/tricaster-remote/static/js/main.b5f3e890.chunk.js"
+  },
+  {
+    "revision": "73f9c67e2d3dbea8a2d0",
+    "url": "/tricaster-remote/static/js/runtime-main.8617dad8.js"
+  }
+]);
